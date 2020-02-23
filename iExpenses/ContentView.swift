da6647c2d1702c65acfc3ef30bcda723e4426634
Vegesna, Vijay V EX1 <VVegesna@us.imshealth.com>
2020-02-22 21:08:12 -0700

    Initial Commit

---
+++ iExpenses/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iExpenses
+//
+//  Created by Vegesna, Vijay V EX1 on 2/22/20.
+//  Copyright © 2020 Vegesna, Vijay V EX1. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
